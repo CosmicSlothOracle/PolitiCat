d1b3b92edc6aa8dca99b9c9122cd0d66f07a6b28
CosmicSlothOracle <LaurinEhinger@proton.me>
2025-05-03 23:24:34 +0200

    WIP: preparing for beat em up quicktime event integration

--- src/game/gameData.ts
+++ src/game/gameData.ts
@@ -2,47 +2,58 @@ import { Card } from './types';
 
 export const allCards: Card[] = [
   {
-    name: "Donald Trump",
-    imagePath: "/cards/DonaldTrump.png",
-    charisma: 9,
+    name: "Gold D. Olaf",
+    imagePath: "/cards/GoldDOlaf.png",
+    charisma: 5,
     leadership: 6,
-    influence: 9,
-    integrity: 2,
-    trickery: 10,
-    wealth: 9,
-    quote: "My fingers are long and beautiful, as, it has been well documented, are various other parts of my body."
-  },
-  {
-    name: "Barack Obama",
-    imagePath: "/cards/BarackObama.png",
-    charisma: 10,
-    leadership: 9,
-    influence: 9,
-    integrity: 8,
-    trickery: 3,
-    wealth: 6,
-    quote: "Yes we can."
-  },
-  {
-    name: "Recep Tayyip Erdogan",
-    imagePath: "/cards/RecepTayyipErdogan.png",
-    charisma: 7,
-    leadership: 8,
-    influence: 8,
+    influence: 7,
     integrity: 3,
-    trickery: 8,
-    wealth: 7,
-    quote: "Democracy is like a streetcar. When you come to your stop, you get off."
+    trickery: 9,
+    wealth: 8,
+    quote: "I have no recollection of that."
+  },
+  {
+    name: "Kim Jong-un",
+    imagePath: "/cards/KimJongun.png",
+    charisma: 6,
+    leadership: 7,
+    influence: 5,
+    integrity: 2,
+    trickery: 7,
+    wealth: 8,
+    quote: "My button works."
+  },
+  {
+    name: "Angelo Merte",
+    imagePath: "/cards/AngeloMerte.png",
+    charisma: 7,
+    leadership: 6,
+    influence: 5,
+    integrity: 8,
+    trickery: 4,
+    wealth: 3,
+    quote: "There is always an alternative."
+  },
+  {
+    name: "Peer Steinbrück",
+    imagePath: "/cards/PeerSteinbrück.png",
+    charisma: 5,
+    leadership: 6,
+    influence: 5,
+    integrity: 6,
+    trickery: 4,
+    wealth: 4,
+    quote: "Markets react to clarity. Not to charisma"
   },
   {
     name: "Karl Lauterbach",
     imagePath: "/cards/KarlLauterbach.png",
     charisma: 6,
     leadership: 6,
-    influence: 6,
-    integrity: 7,
+    influence: 7,
+    integrity: 8,
     trickery: 2,
-    wealth: 4,
+    wealth: 3,
     quote: "Grilled meat is always harmful."
   },
   {
@@ -57,15 +68,37 @@ export const allCards: Card[] = [
     quote: "Wir schaffen das."
   },
   {
-    name: "Greta Thunberg",
-    imagePath: "/cards/GretaThunberg.png",
-    charisma: 6,
-    leadership: 7,
-    influence: 3,
-    integrity: 9,
-    trickery: 2,
-    wealth: 1,
-    quote: "How dare you!"
+    name: "Barack Obama",
+    imagePath: "/cards/BarackObama.png",
+    charisma: 10,
+    leadership: 9,
+    influence: 9,
+    integrity: 8,
+    trickery: 3,
+    wealth: 6,
+    quote: "Yes we can."
+  },
+  {
+    name: "Donald Trump",
+    imagePath: "/cards/DonaldTrump.png",
+    charisma: 9,
+    leadership: 6,
+    influence: 9,
+    integrity: 2,
+    trickery: 10,
+    wealth: 9,
+    quote: "My fingers are long and beautiful, as, it has been well documented, are various other parts of my body."
+  },
+  {
+    name: "Recep Tayyip Erdogan",
+    imagePath: "/cards/RecepTayyipErdogan.png",
+    charisma: 7,
+    leadership: 8,
+    influence: 8,
+    integrity: 3,
+    trickery: 8,
+    wealth: 7,
+    quote: "Democracy is like a streetcar. When you come to your stop, you get off."
   },
   {
     name: "Wolodymyr Selenskyj",
@@ -79,19 +112,8 @@ export const allCards: Card[] = [
     quote: "I am no Hero, I am the President"
   },
   {
-    name: "Kim Jong-un",
-    imagePath: "/cards/Kim.png",
-    charisma: 5,
-    leadership: 6,
-    influence: 5,
-    integrity: 6,
-    trickery: 3,
-    wealth: 4,
-    quote: "I will no longer settle for broken promises."
-  },
-  {
     name: "Vladimir Putin",
-    imagePath: "/cards/WladimirPutin.png",
+    imagePath: "/cards/VladimirPutin.png",
     charisma: 7,
     leadership: 10,
     influence: 9,
@@ -102,7 +124,7 @@ export const allCards: Card[] = [
   },
   {
     name: "Markus Söder",
-    imagePath: "/cards/MarkusSoder.png",
+    imagePath: "/cards/MarkusSöder.png",
     charisma: 7,
     leadership: 7,
     influence: 5,
@@ -110,5 +132,38 @@ export const allCards: Card[] = [
     trickery: 6,
     wealth: 5,
     quote: "Wer nicht mit der Zeit geht, geht mit der Zeit."
+  },
+  {
+    name: "Greta Thunberg",
+    imagePath: "/cards/GretaThunberg.png",
+    charisma: 6,
+    leadership: 7,
+    influence: 3,
+    integrity: 9,
+    trickery: 2,
+    wealth: 1,
+    quote: "How dare you!"
+  },
+  {
+    name: "Papst Paul VI.",
+    imagePath: "/cards/PapstPaulVI.png",
+    charisma: 5,
+    leadership: 6,
+    influence: 4,
+    integrity: 7,
+    trickery: 3,
+    wealth: 3,
+    quote: "If you want peace, work for justice."
+  },
+  {
+    name: "Xi Jinping",
+    imagePath: "/cards/XiJinping.png",
+    charisma: 6,
+    leadership: 8,
+    influence: 9,
+    integrity: 3,
+    trickery: 7,
+    wealth: 9,
+    quote: "The Chinese Dream is the great rejuvenation of the Chinese nation."
   }
 ];
